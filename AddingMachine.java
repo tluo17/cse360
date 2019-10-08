92797dce77544c7f8e12a96be0b28780a7bf4aec
tluo17 <56104930+tluo17@users.noreply.github.com>
2019-10-07 18:07:01 -0700

    final version

--- AddingMachine.java
+++ AddingMachine.java
@@ -1,30 +1,41 @@
+//Tingyu Luo CSE360 Assignment 2
 package cse360assign2;
 
-public class AddingMachine {
-
+public class AddingMachine 
+{
 	private int total;
-	
-	public AddingMachine () {
+	private String history;
+	public AddingMachine ()
+	{
 		total = 0;  // not needed - included for clarity
+		history="0";
 	}
 	
-	public int getTotal () {
-		return 0;
+	public int getTotal () 
+	{
+		return total;
 	}
 	
-	public void add (int value) {
-		
+	public void add (int value) 
+	{
+		total=total+value;
+		history=history+" + "+value;
 	}
 	
-	public void subtract (int value) {
-		
+	public void subtract (int value) 
+	{
+		total=total-value;
+		history=history+" - "+value;
 	}
 		
-	public String toString () {
-		return "";
+	public String toString ()
+	{
+		return history;
 	}
 
-	public void clear() {
-	
+	public void clear() 
+	{
+		total=0;
+		history="";
 	}
 }
